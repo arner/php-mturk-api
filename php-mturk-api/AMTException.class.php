f2e45d47be45ac06ed24b1b98130c4651de4f509
arne <arne123@gmail.com>
2014-01-08 09:51:19 +0100

    rename

---
+++ php-mturk-api/AMTException.class.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     * AMTException
+     * 
+     */
+    class AMTException extends Exception {
+        public function __construct($message, $code = 0, Exception $previous = null) {
+			parent::__construct($message, $code, $previous);
+        }
+    }
+
+
+
+
+?>
